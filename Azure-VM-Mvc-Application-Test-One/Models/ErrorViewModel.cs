a5ed0ac7967b898ccae3ea84afee012d7d430eee
prince prasad <princeprasad.1991@gmail.com>
2021-08-18 15:44:03 +0530

    Add project files.

---
+++ Azure-VM-Mvc-Application-Test-One/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure_VM_Mvc_Application_Test_One.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
